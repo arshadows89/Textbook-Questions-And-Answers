025c342ba97fe5b7990684df5049ff29fbc4f8ba
Cameron Snyder <dooffycom@gmail.com>
2014-10-18 20:36:14 +0000

    Adding the Rock Paper Scissors game to githug

---
+++ Tealeaf_Course_1/Rock_Paper_Scissors.rb
@@ -0,0 +1,70 @@
+###Rock Paper scissor###
+def rock_paper_scissors_game
+  def cpu_answer
+  cpu_answer = rand(3)
+    if cpu_answer == 0
+      @cpu_answer = 'Rock'
+    elsif cpu_answer == 1
+      @cpu_answer = 'Paper'
+    elsif cpu_answer == 2
+      @cpu_answer = 'Scissors'
+    end
+  end
+
+  def winner
+    if  (@answer.downcase == 'rock') && (@cpu_answer.downcase == 'paper')
+      puts 'YOU LOSE'
+    elsif (@answer.downcase == 'paper') && (@cpu_answer.downcase == 'rock')
+      puts 'WINNER'
+    elsif (@answer.downcase == 'paper') && (@cpu_answer.downcase == 'scissors')
+      puts 'You Lose'
+    elsif (@answer.downcase == 'scissors') && (@cpu_answer.downcase == 'paper')
+      puts 'WINNER'
+    elsif (@answer.downcase == 'scissors') && (@cpu_answer.downcase == 'rock')
+      puts 'You Lose'
+    elsif (@answer.downcase == 'rock') && (@cpu_answer.downcase == 'scissors')
+      puts 'Winner'
+    else
+      #anything else here will be a tie thus restart program
+      puts 'You both tied. Go again.'
+      rock_paper_scissors_game
+    end
+  end
+  
+  def play_again
+    puts 'Do you want to play another round of Rock, Paper, Scissors? Yes or No?'
+    play_again_response = gets.chomp
+    while play_again_response != nil
+      if play_again_response.downcase == 'yes'
+        puts "OK! Lets play again!"
+        rock_paper_scissors_game
+      elsif  play_again_response.downcase == 'no'
+        break
+      else
+        puts 'You didnt enter Yes or No. Please type Yes or No if you want to play again.'
+        play_again_response = gets.chomp
+      end
+    end
+  end
+
+  puts 'Rock, Paper, Scissors, Shoot!!!'
+  puts 'What do you choose, Rock, Paper, or Scissors?'
+  @answer = gets.chomp
+
+  while @answer != nil
+    if (@answer.downcase == 'rock') || (@answer.downcase == 'paper') || (@answer.downcase == 'scissors')
+      break
+    else
+      puts 'You entered '+@answer.capitalize+' which isn\'t one of the following words, Scissors, Paper, or Rock.  Please try again.'
+      @answer = gets.chomp
+    end
+  end
+
+  cpu_answer
+  puts 'You chose '+@answer.capitalize+' and the computer chose '+@cpu_answer+'!'
+  winner
+  play_again
+end
+
+puts 'Lets play a game of rock, paper, scissors'
+rock_paper_scissors_game
