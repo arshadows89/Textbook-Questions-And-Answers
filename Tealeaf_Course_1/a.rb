a650a6048e825e3ed89249ee3bea74190eb610ec
Cameron Snyder <dooffycom@gmail.com>
2014-10-21 22:42:13 +0000

    test

--- Tealeaf_Course_1/a.rb
+++ Tealeaf_Course_1/a.rb
@@ -23,6 +23,11 @@
   
 #Runs the whole game of Tic Tac Toe
 def tic_tac_toe
+    
+  player_picks = []
+  computers_picks = []
+  total_picks = []
+  gameboard = {a: ' ', b: ' ', c: ' ', d: ' ', e: ' ', f: ' ', g: ' ', h: ' ', i: ' '}
   
   #Method if the user wants to play again
   def play_again
@@ -44,36 +49,82 @@ def tic_tac_toe
   end
   
   # calculates if someone won
-  def who_won
+  def who_won(player_picks, computers_picks, total_picks)
     # checks to see if user won
-   if user_picks.include?(1) and user_picks.include?(4) and user_picks.include?(7) or user_picks.include?(7) and user_picks.include?(5) and user_picks.include?(3) or user_picks.include?(3) and user_picks.include?(6) and user_picks.include?(9) or user_picks.include?(2) and user_picks.include?(5) and user_picks.include?(8) or user_picks.include?(7) and user_picks.include?(8) and user_picks.include?(9) or user_picks.include?(4) and user_picks.include?(5) and user_picks.include?(6) or user_picks.include?(1) and user_picks.include?(2) and user_picks.include?(3) or user_picks.include?(1) and user_picks.include?(5) and user_picks.include?(9)
+    if player_picks.include?(1) and player_picks.include?(4) and player_picks.include?(7) or player_picks.include?(7) and player_picks.include?(5) and player_picks.include?(3) or player_picks.include?(3) and player_picks.include?(6) and player_picks.include?(9) or player_picks.include?(2) and player_picks.include?(5) and player_picks.include?(8) or player_picks.include?(7) and player_picks.include?(8) and player_picks.include?(9) or player_picks.include?(4) and player_picks.include?(5) and player_picks.include?(6) or player_picks.include?(1) and player_picks.include?(2) and player_picks.include?(3) or player_picks.include?(1) and player_picks.include?(5) and player_picks.include?(9)
      puts 'You WON!!!'
      play_again
      exit
    
      # checks to see if computer won
-   elsif computer_picks.include?(1) and computer_picks.include?(4) and computer_picks.include?(7) or computer_picks.include?(3) and computer_picks.include?(6) and computer_picks.include?(9) or computer_picks.include?(2) and computer_picks.include?(5) and computer_picks.include?(8) or computer_picks.include?(7) and computer_picks.include?(8) and computer_picks.include?(9) or computer_picks.include?(4) and computer_picks.include?(5) and computer_picks.include?(6) or computer_picks.include?(1) and computer_picks.include?(2) and computer_picks.include?(3) or computer_picks.include?(1) and computer_picks.include?(5) and computer_picks.include?(9) or computer_picks.include?(7) and computer_picks.include?(5) and computer_picks.include?(3)
+   elsif computers_picks.include?(1) and computers_picks.include?(4) and computers_picks.include?(7) or computers_picks.include?(3) and computers_picks.include?(6) and computers_picks.include?(9) or computers_picks.include?(2) and computers_picks.include?(5) and computers_picks.include?(8) or computers_picks.include?(7) and computers_picks.include?(8) and computers_picks.include?(9) or computers_picks.include?(4) and computers_picks.include?(5) and computers_picks.include?(6) or computers_picks.include?(1) and computers_picks.include?(2) and computers_picks.include?(3) or computers_picks.include?(1) and computers_picks.include?(5) and computers_picks.include?(9) or computers_picks.include?(7) and computers_picks.include?(5) and computers_picks.include?(3)
      puts 'You Lose'
-     play_again
+      play_again
      exit
      
      #its a tie so ask them to play again
-   else
+    elsif total_picks == [1, 2, 3 ,4 ,5 ,6, 7, 8, 9]
      puts 'You Tied'
      play_again
      exit
+    else
    end
   end
   
+  # calculates what is left to tell the user what he can pick
+  def whats_left (total_picks)
+    possible_picks = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    picks_left = possible_picks - total_picks
+    print 'You still can pick from' 
+    picks_left.each do |left|
+      print ' '+left.to_s+''
+    end
+    puts '.'
+  end
+  
+  # Gets player move, and makes changes to the board
+  def players_turn(gameboard, player_picks, total_picks)
+    board(gameboard)
+    whats_left (total_picks)
+    player_choice = gets.chomp
+    unless total_picks.include? player_choice.to_i
+      player_picks.push player_choice.to_i
+      total_picks.push player_choice.to_i    
+      if player_choice.to_i == 1
+        gameboard[:a] = 'X'
+      elsif player_choice.to_i == 2
+        gameboard[:b] = 'X'
+      elsif player_choice.to_i == 3
+        gameboard[:c] = 'X'
+      elsif player_choice.to_i == 4
+        gameboard[:d] = 'X'
+      elsif player_choice.to_i == 5
+        gameboard[:e] = 'X'
+      elsif player_choice.to_i == 6
+        gameboard[:f] = 'X'
+      elsif player_choice.to_i == 7
+        gameboard[:g] = 'X'
+      elsif player_choice.to_i == 8
+        gameboard[:h] = 'X'
+      elsif player_choice.to_i == 9
+        gameboard[:i] = 'X'
+      end
+    else 
+      puts 'You picked a spot that wasnt available, try again.'
+      players_turn(gameboard, player_picks, total_picks)
+      exit   
+    end
+  end
+  
   #the board
-  def board
-    a, b, c, d, e, f, g, h, i = ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' 
+  def board(gameboard)
+    system 'clear'
 
-    puts ' '+a+' | '+b+' | '+c+' '
+    puts ' '+gameboard[:a]+' | '+gameboard[:b]+' | '+gameboard[:c]+' '
     puts '---|---|---'
-    puts ' '+d+' | '+e+' | '+f+' '
+    puts ' '+gameboard[:d]+' | '+gameboard[:e]+' | '+gameboard[:f]+' '
     puts '---|---|---'
-    puts ' '+g+' | '+h+' | '+i+' '
+    puts ' '+gameboard[:g]+' | '+gameboard[:h]+' | '+gameboard[:i]+' '
   end
   
   #What move the computer will make
@@ -163,40 +214,51 @@ def tic_tac_toe
       end
     end
   end
-  player_picks = []
-  computers_picks = []
-  total_picks = []
-  gameboard = {a: ' ', b: ' ', c: ' ', d: ' ', e: ' ', f: ' ', g: ' ', h: ' ', i: ' '}
+
+  
+  #runs each turn, stops if winner is found though
+  def turns(gameboard, computers_picks, player_picks, total_picks)
+  #9 total turns
+  players_turn(gameboard, player_picks, total_picks)
+  who_won(player_picks, computers_picks, total_picks)
+  computers_turn(gameboard, computers_picks, player_picks, total_picks)
+  who_won(player_picks, computers_picks, total_picks)
+  players_turn(gameboard, player_picks, total_picks)
+  who_won(player_picks, computers_picks, total_picks)
+  computers_turn(gameboard, computers_picks, player_picks, total_picks)
+  who_won(player_picks, computers_picks, total_picks)
+  players_turn(gameboard, player_picks, total_picks)
+  who_won(player_picks, computers_picks, total_picks)
+  computers_turn(gameboard, computers_picks, player_picks, total_picks)
+  who_won(player_picks, computers_picks, total_picks)
+  players_turn(gameboard, player_picks, total_picks)
+  who_won(player_picks, computers_picks, total_picks)
+  computers_turn(gameboard, computers_picks, player_picks, total_picks)
+  who_won(player_picks, computers_picks, total_picks)
+  players_turn(gameboard, player_picks, total_picks)
+  who_won(player_picks, computers_picks, total_picks)
 end
   
-  
+  turns(gameboard, computers_picks, player_picks, total_picks)
+end
+
 puts 'Welcome to Tic-Tac-Toe. '
 #write out more instruction once we develop the game
 tic_tac_toe
 
-#####
-def turns
-  #9 total turns
-  player turn
-  who_won
-  computers_turn(gameboard, computers_picks, player_picks, total_picks)
-  who_won
-  player turn
-  who_won
-  computers_turn(gameboard, computers_picks, player_picks, total_picks)
-  who_won
-  player turn
-  who_won
-  computers_turn(gameboard, computers_picks, player_picks, total_picks)
-  who_won
-  player turn
-  who_won
-  computers_turn(gameboard, computers_picks, player_picks, total_picks)
-  who_won
-  player turn
-  who_won
-end
+
+
 
 # make section for players turn
 # fix the board to hash use gameboard references
 # gameboard and chat doesnt move when entering numbers. (might need to look at how they did that) might be not using puts which pushes to a new line?
+# fix def turns
+# add description for each method
+#need to add board move to player and computers choice
+
+# if no winner decieded we need to decide if ...
+
+  # easier way then long if else statement for both computer and players turn for the telling it what part of gameboard to go to?
+  
+
+    
