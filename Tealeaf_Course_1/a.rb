61b02cbfe96a595fdb7cdb3d006877ad52f03b86
Cameron Snyder <dooffycom@gmail.com>
2014-10-21 03:34:44 +0000

    still working on tic-tac-toe'

--- Tealeaf_Course_1/a.rb
+++ Tealeaf_Course_1/a.rb
@@ -15,6 +15,9 @@
 # all_picks (need to have a array with all pick from both user and computer)
  # computers_turn
     
+#need to redo board so its into hash
+
+#random to see who goes first
 ##################################################
 #Tic Tac Toe Game
   
@@ -74,38 +77,122 @@ def tic_tac_toe
   end
   
   #What move the computer will make
-  def computers_turn
-    #need to create a function for computers action (prob run random but could craete a cpu ai)
-    #random rand(9) to get 9 numbers
+  def computers_turn(gameboard, computers_picks, player_picks, total_picks)
+    # simple random ai for computers picks
     computer_picks = rand(9)
     if computer_picks == 0
-      computer_picks = a
+      if total_picks.include? 1
+        computers_turn(gameboard, computers_picks, player_picks, total_picks)
+        exit
+      else
+        gameboard[:a] = 'O'
+        computers_picks.push 1
+        total_picks.push 1
+      end
     elsif computer_picks == 1
-      computer_picks = b
+      if total_picks.include? 2
+        computers_turn(gameboard, computers_picks, player_picks, total_picks)
+        exit
+      else
+        gameboard[:b] = 'O'
+        computers_picks.push 2
+        total_picks.push 2
+      end
     elsif computer_picks == 2
-      computer_picks = c
+      if total_picks.include? 3
+        computers_turn(gameboard, computers_picks, player_picks, total_picks)
+        exit
+      else
+        gameboard[:c] = 'O'
+        computers_picks.push 3
+        total_picks.push 3
+      end
     elsif computer_picks == 3
-      computer_picks = d
+      if total_picks.include? 4
+        computers_turn(gameboard, computers_picks, player_picks, total_picks)
+        exit
+      else
+        gameboard[:d] = 'O'
+        computers_picks.push 4
+        total_picks.push 4
+      end
     elsif computer_picks == 4
-      computer_picks = e
+      if total_picks.include? 5
+        computers_turn(gameboard, computers_picks, player_picks, total_picks)
+        exit
+      else
+        gameboard[:e] = 'O'
+        computers_picks.push 5
+        total_picks.push 5
+      end
     elsif computer_picks == 5
-      computer_picks = f
+      if total_picks.include? 6
+        computers_turn(gameboard, computers_picks, player_picks, total_picks)
+        exit
+      else
+        gameboard[:f] = 'O'
+        computers_picks.push 6
+        total_picks.push 6
+      end
     elsif computer_picks == 6
-      computer_picks = g
+      if total_picks.include? 7
+        computers_turn(gameboard, computers_picks, player_picks, total_picks)
+        exit
+      else
+        gameboard[:g] = 'O'
+        computers_picks.push 7
+        total_picks.push 7
+      end
     elsif computer_picks == 7
-      computer_picks = h
+      if total_picks.include? 8
+        computers_turn(gameboard, computers_picks, player_picks, total_picks)
+        exit
+      else
+        gameboard[:h] = 'O'
+        computers_picks.push 8
+        total_picks.push 8
+      end
     elsif computer_picks == 8
-      computer_picks = i
+      if total_picks.include? 9
+        computers_turn(gameboard, computers_picks, player_picks, total_picks)
+        exit
+      else
+        gameboard[:i] = 'O'
+        computers_picks.push 9 
+        total_picks.push 9
+      end
     end
-    # if the random number is in the all_picks array then have to redo rand so run computer_turn again (use exit at end)
   end
+  player_picks = []
+  computers_picks = []
+  total_picks = []
+  gameboard = {a: ' ', b: ' ', c: ' ', d: ' ', e: ' ', f: ' ', g: ' ', h: ' ', i: ' '}
 end
-
+  
+  
 puts 'Welcome to Tic-Tac-Toe. '
 #write out more instruction once we develop the game
 tic_tac_toe
 
-
-
-#need to redo board so its into hash
-# need to tell computer_picks to push into the hash at value a if 0 then push O for board pick.
+#####
+def turns
+  #9 total turns
+  player turn
+  who_won
+  computers_turn(gameboard, computers_picks, player_picks, total_picks)
+  who_won
+  player turn
+  who_won
+  computers_turn(gameboard, computers_picks, player_picks, total_picks)
+  who_won
+  player turn
+  who_won
+  computers_turn(gameboard, computers_picks, player_picks, total_picks)
+  who_won
+  player turn
+  who_won
+  computers_turn(gameboard, computers_picks, player_picks, total_picks)
+  who_won
+  player turn
+  who_won
+end
